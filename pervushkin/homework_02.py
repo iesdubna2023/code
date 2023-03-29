25bcb6ffc09d4286190ccf8fd86e460f0a793b4f
ogent_kgb <dimitri102@mail.ru>
2023-03-29 16:16:52 +0300

    commit: pervushkin homework 2

---
+++ pervushkin/homework_02.py
@@ -0,0 +1,54 @@
+def reverse(arg):
+    result = []
+    for i in range(len(arg)):
+        result.append(arg[len(arg) - 1 - i])
+    return result
+
+
+def avglen(arg):
+    summa = 0
+    for i in arg:
+        summa = summa + len(i)
+    return summa / len(arg)
+
+
+def index(arg):
+    result = {}
+    for i in range(len(arg)):
+        if arg[i] not in result.keys():
+            result[arg[i]] = [i]
+        else:
+            result[arg[i]].append(i)
+    for i in result.keys():
+        if len(result[i]) == 1:
+            result[i] = result[i][0]
+    return result
+
+
+def coincidence(arg1, arg2):
+    result = []
+    for i in arg1:
+        if i in arg2:
+            result.append(i)
+    return result
+
+
+def count(arg):
+    result = {}
+    for i in arg:
+        if i not in result.keys():
+            result[i] = 1
+        else:
+            result[i] += 1
+    return result
+
+
+def lensort(arg):
+    n = len(arg)
+    for i in range(n):
+        for j in range(i + 1, n):
+            if len(arg[i]) > len(arg[j]):
+                temp = arg[i]
+                arg[i] = arg[j]
+                arg[j] = temp
+    return arg
