f0789bce9f40cedea962129a457bb3e87110bf34
Vladimir Kozhukalov <kozhukalov@gmail.com>
2023-03-30 22:16:06 +0300

    Modify homework 02 test

--- tests/homework_02_test.py
+++ tests/homework_02_test.py
@@ -58,7 +58,7 @@ def test_index():
 
 def test_coincidence():
     arg1 = ["my", "name", "is", "Masha"]
-    arg2 = ["my", "name", "is", "Zhenya"]
+    arg2 = ["my", "name", "is", "Zhenya", "some"]
     expected = ["my", "name", "is"]
     for surname, m in iter_homework_02():
         logging.info("Test coincidence for %s", surname)
