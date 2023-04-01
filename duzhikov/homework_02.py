5429bacdb4d62ad43fa8b83c296739aceff68457
z1r02 <ddyu.21@uni-dubna.ru>
2023-04-01 03:19:38 +0300

    duzhikov dmitry homework 2

---
+++ duzhikov/homework_02.py
@@ -0,0 +1,71 @@
+arg1 = ["my", "name", "is", "Masha"]
+# resut = ["Masha", "is", "name", "my"]
+def reverse_list(lst):
+    return lst[::-1]
+
+
+arg2 = ["a", "ab", "abc"]
+# result = 2.0
+def average_word_length(lst):
+    return sum(len(word) for word in lst) / len(lst)
+
+
+arg3 = ["her", "name", "is", "Masha", "Masha", "is", "a", "sister", "of", "Zhenya"]
+# result = {
+#     "her": 0,
+#     "name": 1,
+#     "is": [2, 5],
+#     "Masha": [3, 4],
+#     "a": 6,
+#     "sister": 7,
+#     "of": 8,
+#     "Zhenya": 9,
+# }
+from collections import defaultdict
+
+def index_words(lst):
+    indexes = defaultdict(list)
+    for i, word in enumerate(lst):
+        indexes[word].append(i)
+    return {k: v[0] if len(v) == 1 else v for k, v in indexes.items()}
+
+
+arg4_1 = ["my", "name", "is", "Masha"]
+arg4_2 = ["my", "name", "is", "Zhenya"]
+# result = ["my", "name", "is"]
+def intersect_lists(lst1, lst2):
+    return list(set(lst1) & set(lst2))
+
+
+arg5 = ["aaa", "aaa", "bbb", "ccc", "bbb"]
+# result = {
+#     "aaa": 2,
+#     "bbb": 2,
+#     "ccc": 1,
+# }
+from collections import Counter
+
+def count_words(lst):
+    return dict(Counter(lst))
+
+
+arg6 = ["abcd", "a", "ab", "abc", "bazinga", "bar"]
+# result = ["a", "ab", "abc", "bar", "abcd", "bazinga"]
+def sorted_by_Dimasik(list):
+    len_list = len(list)
+    for i in range(1, len_list):
+        data = list[i]
+        count = i - 1
+        while count >= 0 and len(data) < len(list[count]):
+            list[count + 1] = list[count]
+            count -= 1
+        list[count + 1] = data
+    return (list)
+
+
+print(reverse_list(arg1))
+print(average_word_length(arg2))
+print(index_words(arg3))
+print(intersect_lists(arg4_1, arg4_2))
+print(count_words(arg5))
+print(sorted_by_Dimasik(arg6))
