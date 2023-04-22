3fa6e55a8dded1dba79d9e2e3c45a1e939145256
z1r02 <ddyu.21@uni-dubna.ru>
2023-04-22 16:10:15 +0300

    dyuzhikov homework 03

--- duzhikov/homework_03.py
+++ duzhikov/homework_03.py
@@ -20,6 +20,11 @@ class Point2D(Figure2D):
         self.x = x
         self.y = y
 
+    def distance(self, other):
+        dx = self.x - other.x
+        dy = self.y - other.y
+        return (dx ** 2 + dy ** 2) ** 0.5
+
     def area(self):
         return 0.0
 
@@ -55,11 +60,19 @@ class Segment2D(Figure2D):
             self.p2.mirror_point(point)
         )
 
-    def mirror_line(self, point):
-        return Segment2D(
-            self.p1.mirror_point(point),
-            self.p2.mirror_point(point)
-        )
+    def mirror_line(self, p1, p2):
+        m = (p2.y - p1.y) / (p2.x - p1.x)
+        b = p1.y - m * p1.x
+        c = self.p1.x
+        d = self.p1.y
+        e = self.p2.x
+        f = self.p2.y
+        start_x = (c + (2 * m * (d - b))) / (1 + m ** 2)
+        start_y = (2 * m * start_x) + (2 * b) - d
+        end_x = (e + (2 * m * (f - b))) / (1 + m ** 2)
+        end_y = (2 * m * end_x) + (2 * b) - f
+
+        return Segment2D(Point2D(start_x, start_y), Point2D(end_x, end_y))
 
     def belongs_point(self, point):
         z = self.p1.x
