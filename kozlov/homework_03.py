3875ce22c64c487c384254557ad944944ddaa885
KozlovAndrey04 <KoAS.21@uni-dubna.ru>
2023-04-22 18:14:42 +0300

    kozlov homework 3

--- kozlov/homework_03.py
+++ kozlov/homework_03.py
@@ -3,20 +3,10 @@ import math
 
 class Figure2D:
     def __init__(self):
-        self.x = None
-        self.y = None
-        self.p1 = None
-        self.p2 = None
-        self.p3 = None
+        pass
 
     def area(self):
         return 0
-
-    def mirror_point(self, figure):
-        return
-
-    def mirror_line(self, figure):
-        return
 
 
 class Point2D(Figure2D):
@@ -24,29 +14,26 @@ class Point2D(Figure2D):
         self.x = x
         self.y = y
 
-    def mirror_point(self, figure):
-        x, y = figure.x, figure.y
-        self.x = x - (self.x - x)
-        self.y = y - (self.y - y)
+    def mirror_point(self, point):
+        x = 2 * point.x - self.x
+        y = 2 * point.y - self.y
+        return Point2D(x, y)
 
-    def mirror_line(self, figure):
-        x1, y1 = figure.p1.x, figure.p1.y
-        x2, y2 = figure.p2.x, figure.p2.y
+    def mirror_line(self, segment):
+        x1, y1 = segment.p1.x, segment.p1.y
+        x2, y2 = segment.p2.x, segment.p2.y
         x3, y3 = self.x, self.y
         a = (x2 - x1) * (y2 - y1) * (y3 - y1)
         b = x1 * pow(y2 - y1, 2) + x3 * pow(x2 - x1, 2)
         x4 = round((a + b) / (pow(y2 - y1, 2) + pow(x2 - x1, 2)))
         y4 = round((y2 - y1) * (x4 - x1) / (x2 - x1) + y1)
-        self.x = x4 + (x4 - x3)
-        self.y = y4 + (y4 - y3)
+        x = x4 + (x4 - x3)
+        y = y4 + (y4 - y3)
+        return Point2D(x, y)
 
     def belongs_point(self, figure):
         x, y = figure.x, figure.y
-
-        if self.x == x and self.y == y:
-            return True
-        else:
-            return False
+        return self.x == x and self.y == y
 
 
 class Segment2D(Figure2D):
@@ -54,35 +41,18 @@ class Segment2D(Figure2D):
         self.p1 = p1
         self.p2 = p2
 
-    def mirror_point(self, figure):
-        x, y = figure.x, figure.y
-        self.p1.x = x - (self.p1.x - x)
-        self.p1.y = y - (self.p1.y - y)
-        self.p2.x = x - (self.p2.x - x)
-        self.p2.y = y - (self.p2.y - y)
+    def mirror_point(self, point):
+        p1 = self.p1.mirror_point(point)
+        p2 = self.p2.mirror_point(point)
+        return Segment2D(p1, p2)
 
-    def mirror_line(self, figure):
-        x1, y1 = figure.p1.x, figure.p1.y
-        x2, y2 = figure.p2.x, figure.p2.y
+    def mirror_line(self, segment):
+        p1 = self.p1.mirror_line(segment)
+        p2 = self.p2.mirror_line(segment)
+        return Segment2D(p1, p2)
 
-        x3, y3 = self.p1.x, self.p1.y
-        a = (x2 - x1) * (y2 - y1) * (y3 - y1)
-        b = x1 * pow(y2 - y1, 2) + x3 * pow(x2 - x1, 2)
-        x4 = round((a + b) / (pow(y2 - y1, 2) + pow(x2 - x1, 2)))
-        y4 = round((y2 - y1) * (x4 - x1) / (x2 - x1) + y1)
-        self.p1.x = x4 + (x4 - x3)
-        self.p1.y = y4 + (y4 - y3)
-
-        x3, y3 = self.p2.x, self.p2.y
-        a = (x2 - x1) * (y2 - y1) * (y3 - y1)
-        b = x1 * pow(y2 - y1, 2) + x3 * pow(x2 - x1, 2)
-        x4 = round((a + b) / (pow(y2 - y1, 2) + pow(x2 - x1, 2)))
-        y4 = round((y2 - y1) * (x4 - x1) / (x2 - x1) + y1)
-        self.p2.x = x4 + (x4 - x3)
-        self.p2.y = y4 + (y4 - y3)
-
-    def belongs_point(self, figure):
-        x, y = figure.x, figure.y
+    def belongs_point(self, point):
+        x, y = point.x, point.y
         x1, y1 = self.p1.x, self.p1.y
         x2, y2 = self.p2.x, self.p2.y
 
@@ -114,55 +84,24 @@ class Triangle2D(Figure2D):
                       + (self.p1.y - self.p3.y) ** 2)
         p = (a + b + c) / 2
         S = math.sqrt(p * (p - a) * (p - b) * (p - c))
-        return S
+        return round(S)
 
-    def mirror_point(self, figure):
-        x, y = figure.x, figure.y
-        self.p1.x = x - (self.p1.x - x)
-        self.p1.y = y - (self.p1.y - y)
-        self.p2.x = x - (self.p2.x - x)
-        self.p2.y = y - (self.p2.y - y)
-        self.p3.x = x - (self.p3.x - x)
-        self.p3.y = y - (self.p3.y - y)
+    def mirror_point(self, point):
+        p1 = self.p1.mirror_point(point)
+        p2 = self.p2.mirror_point(point)
+        p3 = self.p3.mirror_point(point)
+        return Triangle2D(p1, p2, p3)
 
-    def mirror_line(self, figure):
-        x1, y1 = figure.p1.x, figure.p1.y
-        x2, y2 = figure.p2.x, figure.p2.y
+    def mirror_line(self, segment):
+        p1 = self.p1.mirror_line(segment)
+        p2 = self.p2.mirror_line(segment)
+        p3 = self.p3.mirror_line(segment)
+        return Triangle2D(p1, p2, p3)
 
-        x3, y3 = self.p1.x, self.p1.y
-        a = (x2 - x1) * (y2 - y1) * (y3 - y1)
-        b = x1 * pow(y2 - y1, 2) + x3 * pow(x2 - x1, 2)
-        x4 = round((a + b) / (pow(y2 - y1, 2) + pow(x2 - x1, 2)))
-        y4 = round((y2 - y1) * (x4 - x1) / (x2 - x1) + y1)
-        self.p1.x = x4 + (x4 - x3)
-        self.p1.y = y4 + (y4 - y3)
-
-        x3, y3 = self.p2.x, self.p2.y
-        a = (x2 - x1) * (y2 - y1) * (y3 - y1)
-        b = x1 * pow(y2 - y1, 2) + x3 * pow(x2 - x1, 2)
-        x4 = round((a + b) / (pow(y2 - y1, 2) + pow(x2 - x1, 2)))
-        y4 = round((y2 - y1) * (x4 - x1) / (x2 - x1) + y1)
-        self.p2.x = x4 + (x4 - x3)
-        self.p2.y = y4 + (y4 - y3)
-
-        x3, y3 = self.p3.x, self.p3.y
-        a = (x2 - x1) * (y2 - y1) * (y3 - y1)
-        b = x1 * pow(y2 - y1, 2) + x3 * pow(x2 - x1, 2)
-        x4 = round((a + b) / (pow(y2 - y1, 2) + pow(x2 - x1, 2)))
-        y4 = round((y2 - y1) * (x4 - x1) / (x2 - x1) + y1)
-        self.p3.x = x4 + (x4 - x3)
-        self.p3.y = y4 + (y4 - y3)
-
-    def belongs_point(self, figure):
-        x, y = figure.x, figure.y
-        xp = (self.p1.x, self.p2.x, self.p3.x)
-        yp = (self.p1.y, self.p2.y, self.p3.y)
-        answer = False
-        for i in range(len(xp)):
-            a = (yp[i] <= y and y < yp[i - 1])
-            b = (yp[i - 1] <= y and y < yp[i])
-            c = (xp[i - 1] - xp[i])
-            d = (y - yp[i]) / (yp[i - 1] - yp[i]) + xp[i]
-            if ((a or b) and x > (c * d)):
-                answer = not answer
-        return answer
+    def belongs_point(self, point):
+        # Если сумма площадей треугольников с новой точкой
+        # равно площади треугольника, значит эта точка принадлежит ему
+        t1 = Triangle2D(self.p1, self.p2, point)
+        t2 = Triangle2D(self.p1, self.p3, point)
+        t3 = Triangle2D(self.p2, self.p3, point)
+        return not (t1.area() + t2.area() + t3.area() > self.area())
