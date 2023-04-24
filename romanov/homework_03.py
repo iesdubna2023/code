0e30b4e9d9f12f94b156007a63ec59877fea12e3
Ivan Romanov <105872764+LooneStalkeR@users.noreply.github.com>
2023-04-25 00:20:43 +0300

    romanov homework 03

--- romanov/homework_03.py
+++ romanov/homework_03.py
@@ -26,15 +26,17 @@ class Point2D(Figure2D):
         return Point2D(result_x, result_y)
 
     def mirror_line(self, mirror_line):
-        p_1 = mirror_line.p1
-        p_2 = mirror_line.p2
-        if p_2.x - p_1.x != 0:
-            k = (p_2.y - p_1.y) / (p_2.x - p_1.x)
-            b = (p_2.x * p_1.y + p_1.x * p_2.y) / (p_2.x - p_1.x)
-            d = (self.x + (self.y - b) * k) / (1 + k ** 2)
-            return Point2D(2 * d - self.x, 2 * d * k - self.y + 2 * b)
-        else:
-            return Point2D(2 * p_1.x - self.x, self.y)
+        p = [self.x, self.y]
+        a = mirror_line.p1.y - mirror_line.p2.y
+        b = mirror_line.p2.x - mirror_line.p1.x
+        c = - (a * mirror_line.p1.x + b * mirror_line.p1.y)
+        term1 = b ** 2 - a ** 2
+        term12 = -2 * a * b
+        term2 = a ** 2 - b ** 2
+        return Point2D((term1 * p[0] + term12 * p[1] - 2 * c * a)
+                       / (a ** 2 + b ** 2),
+                       (term12 * p[0] + term2 * p[1] - 2 * c * b)
+                       / (a ** 2 + b ** 2))
 
 
 class Segment2D(Figure2D):
@@ -55,14 +57,12 @@ class Segment2D(Figure2D):
                          self.p2.mirror_line(mirror_line))
 
     def belongs_point(self, belong_point):
-        if ((belong_point.x - self.p1.x)
+        return ((belong_point.x - self.p1.x)
                 * (self.p2.y - self.p1.y)
                 - (self.p2.x - self.p1.x)
                 * (belong_point.y - self.p1.y) == 0
                 and (self.p1.x < belong_point.x < self.p2.x
-                     or self.p2.x < belong_point.x < self.p1.x)):
-            return True
-        return False
+                     or self.p2.x < belong_point.x < self.p1.x))
 
 
 class Triangle2D(Figure2D):
@@ -88,10 +88,9 @@ class Triangle2D(Figure2D):
                           self.p3.mirror_line(segment))
 
     def belongs_point(self, point):
-        triangle = Triangle2D(self.p1, self.p2, self.p3)
         triangle1 = Triangle2D(self.p1, self.p2, point)
         triangle2 = Triangle2D(self.p1, point, self.p3)
         triangle3 = Triangle2D(point, self.p2, self.p3)
         sm = triangle1.area() + triangle2.area() + triangle3.area()
-        orig = triangle.area()
+        orig = self.area()
         return math.isclose(sm, orig)
