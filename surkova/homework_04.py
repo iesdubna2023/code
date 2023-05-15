d0d9eaa04ad92783515141cda86eec14302a7e6f
SurkovaAnna <99186443+SurkovaAnna@users.noreply.github.com>
2023-05-15 11:40:56 +0300

    surkova homework 04

--- surkova/homework_04.py
+++ surkova/homework_04.py
@@ -1,69 +1,65 @@
 import json
 
 
-# 1
-def dict_comrehension(lst):
-    s = {}
-    for i in range(len(lst)):
-        if len(lst[i]) >= 2:
-            s[lst[i][1]] = lst[i][0]
-
-    return s
+def ll2dict(arg):
+    return {i[1]: i[0] for i in arg if len(i) >= 2}
 
 
-# 2
-class Studend:
+class Student:
     def __init__(self, first_name, second_name, major_subject):
         self.first_name = first_name
         self.second_name = second_name
         self.major_subject = major_subject
 
     def print(self):
-        print(f"Name: {self.first_name}"
-              + f"Surname: {self.second_name}"
-              + f"Major: {self.major_subject}")
+        print(f"Name: {self.first_name} Surname: {self.second_name} "
+              f"Major: {self.major_subject}")
 
 
-def print_std(*args):
-    if len(args) == 1 and type(args[0]) == Studend:
-        args[0].print()
-    elif len(args) == 2:
-        std = Studend(str(args[0]), str(args[1]), 'engineering')
-        std.print()
-    elif len(args) == 3:
-        std = Studend(str(args[0]), str(args[1]), str(args[2]))
-        std.print()
-
-
-# 3
-class Busket:
-    s = {}
-    total = 0
-
-    def __add__(self, other):
-        if type(other) == Item:
-            self.s[other.name] = other.value
-            self.total += other.value
-            return self
-
-    def __sub__(self, other):
-        if type(other) == Item:
-            if other.name in self.s:
-                del self.s[other.name]
-                self.total -= other.value
-                return self
-
-    def __str__(self):
-        return f'{self.s} Total: {self.total}'
+def print_student(first_name='', second_name='',
+                  major_subject='engineering', student=None):
+    if student is not None:
+        student.print()
+        return
+    elif first_name != '' and second_name != '':
+        Student(first_name, second_name, major_subject).print()
+        return
+    raise Exception("No arguments")
 
 
 class Item:
-    def __init__(self, name, value):
+    def __init__(self, name, price):
         self.name = name
-        self.value = value
+        self.price = price
 
 
-# 4
+class Basket:
+    def __init__(self):
+        self.items = {}
+
+    def __add__(self, item):
+        if item.name in self.items:
+            self.items[item.name] += item.price
+        else:
+            self.items[item.name] = item.price
+        return self
+
+    def __sub__(self, item):
+        if item.name in self.items:
+            self.items[item.name] -= item.price
+            if self.items[item.name] <= 0:
+                del self.items[item.name]
+        else:
+            raise ValueError(f"{item.name} not in basket")
+        return self
+
+    def __str__(self):
+        items_str = ", ".join([f"{name}: {price:.2f}" for name,
+                               price in self.items.items()])
+        total = sum(self.items.values())
+        return f"{items_str} Total: {total:.2f}"
+
+
 class DB:
     def __init__(self, file_name):
         self.file = file_name
@@ -90,7 +86,6 @@ class DB:
         return self.data[key]
 
 
-# 5
 def bread(inside):
     def func(item):
         print("bread")
@@ -108,9 +103,9 @@ def mayonnaise(inside):
     return func
 
 
-def vegitables(inside):
+def vegetables(inside):
     def func(item):
-        print("tomatos")
+        print("tomatoes")
         inside(item)
         print("salad")
 
@@ -119,6 +114,6 @@ def vegitables(inside):
 
 @bread
 @mayonnaise
-@vegitables
+@vegetables
 def sandwich(item):
     print(item)
